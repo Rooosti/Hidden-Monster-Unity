b7c044b815e5f9b60d967d5f31ab0380c580fa44
Rooosti <58640924+Rooosti@users.noreply.github.com>
2019-12-07 13:22:59 -0800

    Add files via upload

---
+++ changeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    public Color startColor;
+    public Color hoverColor;
+    bool mouseOver = false;
+
+    void OnMouseEnter()
+    {
+        mouseOver = true;
+        GetComponent<Renderer>().material.SetColor("_Color", hoverColor);
+    }
+
+    void OnMouseExit()
+    {
+        mouseOver = false;
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+    }
+}
---
+++ mouseClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseClick : MonoBehaviour
+{
+    public Vector3 telePosition;
+    public GameObject Player;
+    void OnMouseDown()
+        {
+            Input.GetMouseButtonDown(0);
+            {
+                Player.transform.position = telePosition;
+        }
+    }
+}
